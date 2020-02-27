d63d567bb603051b4d183d582cae45c0f76b1da2
SeongJae Park <sjpark@amazon.de>
2020-02-27 11:48:23 +0000

    correctness/run: Use _summary_results
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- correctness/run.sh
+++ correctness/run.sh
@@ -20,23 +20,14 @@ ksft_abs_path=$LINUX_DIR/$ksft_dir
 $BINDIR/_install.sh $LINUX_DIR
 
 # run
-cd $LINUX_DIR
-make -silent -C $ksft_dir/../damon run_tests | tee $LOG
-make -silent -C $ksft_dir/ run_tests | tee -a $LOG
+(
+	cd $LINUX_DIR
+	make -silent -C $ksft_dir/../damon run_tests | tee $LOG
+	make -silent -C $ksft_dir/ run_tests | tee -a $LOG
+
+	echo "# kselftest dir '$ksft_abs_path' is in dirty state."
+	echo "# the log is at '$LOG'."
+)
 
 # print results
-echo
-grep -q -e '^not ok' $LOG
-if [ $? -ne 0 ]
-then
-	echo -e "\e[92mPASS"
-else
-	echo -e "\e[93mFailed tests:"
-	grep -e '^not ok' $LOG
-	echo
-	echo -e "\e[91mFAIL"
-fi
-echo -e "\e[39m"
-
-echo "# kselftest dir '$ksft_abs_path' is in dirty state."
-echo "# the log is at '$LOG'."
+$BINDIR/_summary_results.sh
