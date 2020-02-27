c959e683a86afb450af38d298ed97cb7659dbfc8
SeongJae Park <sjpark@amazon.de>
2020-02-27 09:54:11 +0000

    performance/run: Simplify code
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- performance/run.sh
+++ performance/run.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
 LBX=$HOME/lazybox
+BINDIR=`dirname $0`
 
-EXPERIMENTS=`dirname $0`
-
-parsec_workloads="blackscholes bodytrack canneal dedup facesim ferret "
-parsec_workloads+="fluidanimate freqmine raytrace streamcluster swaptions "
-parsec_workloads+="vips x264"
-
-splash2x_workloads="barnes fft lu_cb lu_ncb ocean_cp ocean_ncp radiosity "
-splash2x_workloads+="radix raytrace volrend water_nsquared water_spatial"
-
-workloads=""
-for w in $parsec_workloads
-do
-	workloads+="parsec3/$w "
-done
-
-for w in $splash2x_workloads
-do
-	workloads+=" splash2x/$w"
-done
-
-VARIANTS=""
-
-for v in orig rec thp ethp
-do
-	for w in $workloads
-	do
-		VARIANTS+="$w/$v "
-	done
-done
-
-EXPERIMENTS=$EXPERIMENTS VARIANTS=$VARIANTS $LBX/repeat_runs/run.sh
+time CFG=$BINDIR/full_config.sh $LBX/repeat_runs/run.sh
