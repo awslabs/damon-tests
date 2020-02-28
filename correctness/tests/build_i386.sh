d715cb5916a12a9c96c4c8e9c96ee39b8d334616
SeongJae Park <sjpark@amazon.de>
2020-02-28 09:44:00 +0000

    correctness: Add minimized i386 build test
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ correctness/tests/build_i386.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Test i386 build failure problem reported[1] by kbuild robot
+#
+# [1] https://lore.kernel.org/linux-mm/202002051834.cKoViGVl%25lkp@intel.com/
+
+LINUX_SRC='../../../../'
+ODIR=$PWD/`basename $0`.out
+
+mkdir -p $ODIR
+
+cd $LINUX_SRC
+make O=$ODIR ARCH=i386 allnoconfig
+echo 'CONFIG_DAMON=y' >> $ODIR/.config
+echo 'CONFIG_DAMON_KUNIT_TEST=y' >> $ODIR/.config
+make O=$ODIR ARCH=i386 -j`grep -e '^processor' /proc/cpuinfo | wc -l`
