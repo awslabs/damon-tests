58e52c89df8bf9f37901d9e74da68234a21a1d00
SeongJae Park <sjpark@amazon.de>
2020-03-02 09:00:58 +0000

    Revert "performance/parsers/commlog: Calculate and record runtime overhead"
    This reverts commit 1f955f44389255ef89ebd8e8f1c1979851fba571.
    
    Calculating overhead for each of the run seems unnecessary overhead.
    Let's do it in report step.

--- performance/parsers/commlog.sh
+++ performance/parsers/commlog.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-raw_dir=$1
-
 # input is in format of: 'real    0m50.592s'
 runtime=$(grep -e '^real' $1/commlog | sed 's/^real//' | \
 	awk -F'[ms]' '{print $1 * 60 + $2}')
 echo "runtime: $runtime" > $2/runtime
-
-# $1 is <...>/<workload>/(orig|rec|thp|ethp)/[0-9][0-9]
-orig_raw_commlog=$raw_dir/../../orig/`basename $raw_dir`/commlog
-if [ -f $orig_raw_commlog ]
-then
-	orig_runtime=$(grep -e '^real' $orig_raw_commlog | sed 's/^real//' | \
-		awk -F'[ms]' '{print $1 * 60 + $2}')
-	overhead=`awk -v a="$runtime" -v b="$orig_runtime" \
-		'BEGIN{print (a / b - 1) * 100}'`
-	echo "overhead: $overhead" > $2/overhead_runtime
-fi
