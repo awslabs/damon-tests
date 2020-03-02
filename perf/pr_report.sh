d9a2a57861e5ddd0b2c9759cb3cafba4a520d3bb
SeongJae Park <sjpark@amazon.de>
2020-03-02 09:26:44 +0000

    perf/pr_report: Print overheads first
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- perf/pr_report.sh
+++ perf/pr_report.sh
@@ -2,6 +2,26 @@
 
 BINDIR=`dirname $0`
 
+echo "========="
+echo "Overheads"
+echo "========="
+echo
+
+for stat in avg
+do
+	for metric in runtime memused.avg
+	do
+		$BINDIR/_pr_overheads.sh $stat $metric
+		echo
+		echo
+	done
+done
+
+echo "==========="
+echo "Raw Numbers"
+echo "==========="
+echo
+
 for stat in avg stdev min max
 do
 	for metric in runtime memused.avg
