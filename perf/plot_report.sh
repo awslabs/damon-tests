2c9dc17a262c02d97501b6ef6afd8483c3a6a414
SeongJae Park <sjpark@amazon.de>
2020-03-04 08:39:05 +0000

    perf/plot_report: Use cmd argument as output dir path
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- perf/plot_report.sh
+++ perf/plot_report.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <image output dir>"
+	exit 1
+fi
+
+ODIR=$1
+
 BINDIR=`dirname $0`
 LBX=$HOME/lazybox
 PLOT=$LBX/gnuplot/plot.py
 
-REPORTS_DIR=reports/plots/
-mkdir -p $REPORTS_DIR
+mkdir -p $ODIR
 
 for metric in runtime memused.avg
 do
-	OUTPUT_IMG=$REPORTS_DIR/$metric.pdf
+	OUTPUT_IMG=$ODIR/$metric.pdf
 	$BINDIR/_pr_overheads.sh avg $metric | \
 		$PLOT --stdin --type clustered_boxes --xtics_rotate -90 \
 			--ytitle "$metric overhead\n(percent)" \
