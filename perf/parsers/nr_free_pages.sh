548542157dabd104f4f6b976cd3d6f27706e3e8e
SeongJae Park <sjpark@amazon.de>
2021-05-04 09:27:14 +0200

    perf: Add parsers and statists for nr_free_pages
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ perf/parsers/nr_free_pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LBX=$HOME/lazybox
+outfile="nr_used_pages"
+in_dir=$1
+out_dir=$2
+
+"$LBX/scripts/report/memfree_to_used.py" "$in_dir/nr_free_pages" \
+	> "$out_dir/$outfile"
+
+sum=0
+nr=0
+for footprint in $(awk '{print $2}' "$out_dir/$outfile")
+do
+	sum=$((sum + footprint))
+	nr=$((nr + 1))
+done
+echo "$outfile.avg: " $((sum / nr)) > "$out_dir/$outfile.avg"
