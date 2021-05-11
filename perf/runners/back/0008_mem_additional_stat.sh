7eba9218a76626555decd40d057249ad46236d65
SeongJae Park <sjpark@amazon.de>
2021-05-11 14:53:29 +0000

    perf/runners/back/mem_additional_stat: Collect number of page faults
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- perf/runners/back/0008_mem_additional_stat.sh
+++ perf/runners/back/0008_mem_additional_stat.sh
@@ -4,5 +4,7 @@ while :
 do
 	cat /proc/meminfo | grep MemAvailable >> $1/memavail;
 	cat /proc/meminfo | grep SwapCached >> $1/swapcached;
+	grep pgfault /proc/vmstat >> "$1/pgfaults"
+	grep pgmajfault /proc/vmstat >> "$1/pgmajfaults"
 	sleep 1;
 done
