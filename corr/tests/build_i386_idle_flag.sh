30a7c7e85da0e8037ce29ef15d6e6738ff89ac50
SeongJae Park <sjpark@amazon.de>
2021-08-10 09:57:24 +0000

    corr: Add a test for non-64BIT PAGE_IDLE_FLAG only config build test
    This commit adds a build test for a reported issue[1,2].
    
    [1] https://lore.kernel.org/linux-fsdevel/20210806095153.6444-1-sj38.park@gmail.com/
    [2] https://lore.kernel.org/linux-mm/20210809083911.16321-1-sjpark@amazon.de/
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ corr/tests/build_i386_idle_flag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Test i386 build failure problem reported[1] by kbuild robot
+#
+# [1] https://lore.kernel.org/linux-mm/202002051834.cKoViGVl%25lkp@intel.com/
+
+LINUX_SRC='../../../../'
+ODIR=$PWD/`basename $0`.out
+
+mkdir -p $ODIR
+
+cd $LINUX_SRC
+make O=$ODIR ARCH=i386 allnoconfig
+echo 'CONFIG_PAGE_IDLE_FLAG=y' >> $ODIR/.config
+make O=$ODIR ARCH=i386 olddefconfig
+make O=$ODIR ARCH=i386 -j`grep -e '^processor' /proc/cpuinfo | wc -l`
+exit $?
