4400e89d5575ef7f7b07b233dad294298eb74ac2
SeongJae Park <sj@kernel.org>
2021-12-28 22:11:30 +0000

    corr/dbgfs_target_ids_pid_leak: Clean up target_ids file after test
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- corr/tests/dbgfs_target_ids_pid_leak.sh
+++ corr/tests/dbgfs_target_ids_pid_leak.sh
@@ -7,6 +7,8 @@ timeout 1 ./dbgfs_target_ids_pid_leak
 
 after=$(grep "^pid " /proc/slabinfo | awk '{print $2}')
 
+echo > /sys/kernel/debug/damon/target_ids
+
 echo "number of active pid slabs: $before -> $after"
 if [ $after -gt $before ]
 then
