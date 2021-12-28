9ddd9d9fcc86a318186049d5fe1a474b166fe4a8
SeongJae Park <sj@kernel.org>
2021-12-28 16:08:43 +0000

    corr: Add a test for target_ids pid leak issue
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ corr/tests/dbgfs_target_ids_pid_leak.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+before=$(script -q -c "stty rows 999; sudo slabtop -o" /dev/null | grep pid | \
+	awk '{print $2}')
+
+timeout 1 ./dbgfs_target_ids_pid_leak
+
+after=$(script -q -c "stty rows 999; sudo slabtop -o" /dev/null | grep pid | \
+	awk '{print $2}')
+
+echo "number of active pid slabs: $before -> $after"
+if [ $after -gt $before ]
+then
+	echo "maybe pids are leaking"
+	exit 1
+else
+	exit 0
+fi
