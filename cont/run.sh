4a85e21bdb04f9aff2c1516157faf9be9ecf0fa9
SeongJae Park <sj38.park@gmail.com>
2022-04-09 08:33:26 +0000

    cont: Make run as _run and add a wrapper for _run
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- cont/run.sh
+++ cont/run.sh
@@ -1,39 +1,4 @@
 #!/bin/bash
 
-set -e
-
 bindir=$(dirname "$0")
-
-lazybox_dir="$bindir/lazybox"
-
-lazybox_gh_repo="https://github.com/sjp38/lazybox"
-
-if [ ! -d "$lazybox_dir" ]
-then
-	mkdir -p "$lazybox_dir"
-	git -C "$lazybox_dir" init
-	git -C "$lazybox_dir" remote add origin "$lazybox_gh_repo"
-fi
-if ! git -C "$lazybox_dir" pull origin master
-then
-	echo "lazybox pull failed"
-	exit 1
-fi
-
-"$lazybox_dir/humble_ci/hci.py" --repo ./linux \
-	--tree_to_track stable \
-		git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
-		linux-5.15.y \
-	--tree_to_track stable \
-		git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
-		linux-5.16.y \
-	--tree_to_track stable \
-		git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
-		linux-5.17.y \
-	--tree_to_track linus \
-		git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git \
-		master \
-	--tree_to_track akpm-mm \
-		https://github.com/hnaz/linux-mm master \
-	--cmds "$bindir/_for_tree_updates.sh" \
-	--delay 3600
+"$bindir/_run.sh"
