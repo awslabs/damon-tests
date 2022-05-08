c241340bdb60a8d4d569572d61f59419fb7dc007
SeongJae Park <sj38.park@gmail.com>
2022-05-07 20:47:04 -0700

    perf: Add parsers and statists for psi_mem
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ perf/parsers/psi_mem.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+psi_mem_log="$1/psi_mem"
+out_dir="$2"
+
+# example content of log:
+# some avg10=0.00 avg60=0.00 avg300=0.00 total=0
+# full avg10=0.00 avg60=0.00 avg300=0.00 total=0
+# some avg10=0.00 avg60=0.00 avg300=0.00 total=0
+# full avg10=0.00 avg60=0.00 avg300=0.00 total=0
+# some avg10=0.00 avg60=0.00 avg300=0.00 total=4278
+# [...]
+
+some_start=$(head -n 1 "$psi_mem_log" | awk -F '=' '{print $5}')
+full_start=$(head -n 2 "$psi_mem_log" | tail -n 1 | awk -F '=' '{print $5}')
+some_end=$(tail -n 2 "$psi_mem_log" | head -n 1 | awk -F '=' '{print $5}')
+full_end=$(tail -n 1 "$psi_mem_log" | awk -F '=' '{print $5}')
+
+echo $((some_end - some_start)) > "$out_dir/psi_mem_some_us"
+echo $((full_end - full_start)) > "$out_dir/psi_mem_full_us"
