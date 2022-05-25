1f2e90d8a838d6d8bc4d69e67512c128682c80f5
sjpark <sjpark@amazon.com>
2022-05-25 12:38:36 -0700

    perf/parsers/psi_mem: Follow the standard format
    Signed-off-by: sjpark <sjpark@amazon.com>

--- perf/parsers/psi_mem.sh
+++ perf/parsers/psi_mem.sh
@@ -16,5 +16,5 @@ full_start=$(head -n 2 "$psi_mem_log" | tail -n 1 | awk -F '=' '{print $5}')
 some_end=$(tail -n 2 "$psi_mem_log" | head -n 1 | awk -F '=' '{print $5}')
 full_end=$(tail -n 1 "$psi_mem_log" | awk -F '=' '{print $5}')
 
-echo $((some_end - some_start)) > "$out_dir/psi_mem_some_us"
-echo $((full_end - full_start)) > "$out_dir/psi_mem_full_us"
+echo "ps_mem_some_us: $((some_end - some_start))" > "$out_dir/psi_mem_some_us"
+echo "ps_mem_full_us: $((full_end - full_start))" > "$out_dir/psi_mem_full_us"
