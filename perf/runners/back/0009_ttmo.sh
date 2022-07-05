10cd6b9fed17e414a6bb380af04cf8f92d6309eb
sjpark <sj38.park@gmail.com>
2022-07-05 10:32:57 -0700

    perf/runners/back/0009_ttmo: Use more aggressive TMO parameters
    The paper's default parameters for TMO is too conservative so that it
    does not incur performance degradation but also doesn't save memory much
    for the test workloads.  This commit makes it to use more aggressive
    parameters.
    
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- perf/runners/back/0009_ttmo.sh
+++ perf/runners/back/0009_ttmo.sh
@@ -11,6 +11,8 @@ then
 fi
 
 # for paper default param, it should be
-"$bindir/_ttmo.sh" 5 10 6000
+# "$bindir/_ttmo.sh" 5 10 6000
 # we use 10% reclaim_ratio, 1% PSI threshold, 1 sec reclaim interval
 # "$bindir/_ttmo.sh" 1000 100 1000
+# 5% reclaim_ratio, 1% PSI threshold, 1 sec reclaim interval
+"$bindir/_ttmo.sh" 500 100 1000
