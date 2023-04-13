5b652ee9afa2197a305c7010188e24130e4725e3
SeongJae Park <sjpark@amazon.com>
2023-04-13 16:08:42 -0700

    cont/_remote_run_corr: Repeat up to five times with one hour timeout
    DAMON correctiveness test sometimes stuck due to some perf related
    signal handling.  Set one hour timeout and retry the test up to five
    times while rebooting for each timeout.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.com>

--- cont/_remote_run_corr.sh
+++ cont/_remote_run_corr.sh
@@ -16,8 +16,21 @@ ssh_port=$3
 log_file=$4
 work_dir=$5
 
-if ssh "$test_user@$test_machine" -p "$ssh_port" -o ServerAliveInterval=60 \
-	nohup sudo "$work_dir/damon-tests/corr/run.sh" 2>&1 | tee "$log_file"
-then
-	echo "$(basename "$0") SUCCESS" | tee --append "$log_file"
-fi
+timeout_interval=3600
+for i in {1..5};
+do
+	if ssh "$test_user@$test_machine" -p "$ssh_port" \
+		-o ServerAliveInterval=60 \ nohup \
+		sudo timeout "$timeout_interval" \
+			"$work_dir/damon-tests/corr/run.sh" 2>&1 | \
+				tee "$log_file"
+	then
+		echo "$(basename "$0") SUCCESS" | tee --append "$log_file"
+		break
+	fi
+	echo "$(basename "$0") time out ($i times)" | tee --append "$log_file"
+	ssh -p "$ssh_port" "$test_user@$test_machine" \
+		sudo shutdown -r now 2>&1 | tee --append "$log_file"
+	sleep 60
+	echo "shutdown and waitied one minute.  retry..."
+done
