awslabs/damon-tests

commit 727601352b9e8ebc7146a42cabc22d832d719cfe

corr/_summary_results: Do not count skipped test as failure
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

corr/_summary_results.sh
@@ -12,10 +12,12 @@ LOG=$1
 echo -e "\e[32m"
 grep -e '^ok' $LOG
 echo -e "\e[33m"
-grep -e '^not ok' $LOG
-failed=$?
 
-if [ $failed -eq 0 ]
+nr_failures=$(grep -e '^not ok' "$LOG" | wc -l)
+nr_skip=$(grep -e '^not ok' "$LOG" | grep -e '# SKIP$' | wc -l)
+nr_failures=$((nr_failures - nr_skip))
+
+if [ "$nr_failures" -gt 0 ]
 then
 	echo
 	echo -e "\e[91mFAIL\e[39m"

commit ed02b57b15f64b489ecbc57aa523263d29bddef3

corr/_summary_results: Show failed tests list
Commit 727601352b9e ("corr/_summary_results: Do not count skipped test
as failure") hides the failed tests list from the summary's bottom,
which is useful for email notification.  Restore it.

Fixes: 727601352b9e ("corr/_summary_results: Do not count skipped test as failure")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

corr/_summary_results.sh
@@ -13,6 +13,8 @@ echo -e "\e[32m"
 grep -e '^ok' $LOG
 echo -e "\e[33m"
 
+grep -e '^not ok' "$LOG"
+
 nr_failures=$(grep -e '^not ok' "$LOG" | wc -l)
 nr_skip=$(grep -e '^not ok' "$LOG" | grep -e '# SKIP$' | wc -l)
 nr_failures=$((nr_failures - nr_skip))