d5286dc650fad42ea165e0c1e896fc71ad13718d
SeongJae Park <sj38.park@gmail.com>
2023-09-28 22:55:35 +0000

    corr/install_deps: Support ubuntu 22.04
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- corr/install_deps.sh
+++ corr/install_deps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y xz-utils lftp python &> /dev/null
+if ! sudo apt install -y xz-utils lftp python &> /dev/null
+then
+	# python package is not in Ubuntu 22.04
+	sudo apt install -y xz-utils lftp python-is-python3
+fi
