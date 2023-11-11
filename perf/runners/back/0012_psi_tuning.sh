35a157e309ea23832679a4fb1db87bc971077dc0
SeongJae Park <sj38.park@gmail.com>
2023-11-11 19:52:48 +0000

    runners/back/0012_psi_tuning: Aim 1% last 10sec memory some PSI
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- perf/runners/back/0012_psi_tuning.sh
+++ perf/runners/back/0012_psi_tuning.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do feedback-based quota autotune for "*-auto" variants, aiming 1% last 10
+# secs memory some PSI
+
 # $1: <...>/results/<exp>/<variance>/0(0-9)
 bindir=$(dirname "$0")
 odir=$1
@@ -14,22 +17,17 @@ sysfs_dir="/sys/kernel/mm/damon/admin/kdamonds/0/"
 goals_dir="$sysfs_dir/contexts/0/schemes/0/quotas/goals/"
 goal_dir="$sysfs_dir/contexts/0/schemes/0/quotas/goals/0"
 
-last_psi=""
 while :;
 do
-	now_psi=$(cat /proc/pressure/memory | grep some | awk '{print $5}' | \
-		awk -F= '{print $2}')
-	if [ ! "$last_psi" = "" ]
+	if [ ! -d "$goal_dir" ]
 	then
-		# 1% (10ms per second) PSI is the goal
-		if [ ! -d "$goal_dir" ]
-		then
-			echo 1 > "$goals_dir/nr_goals"
-		fi
-		echo 10000 > "$goal_dir/target_value"
-		echo $((now_psi - last_psi)) > "$goal_dir/current_value"
-		echo commit_schemes_quota_goals > "$sysfs_dir/state"
+		echo 1 > "$goals_dir/nr_goals"
 	fi
-	last_psi=$now_psi
+
+	now_psi_bp=$(cat /proc/pressure/memory | grep some | awk '{print $2}' | \
+		awk -F= '{print $2 * 100}')
+	echo 100 > "$goal_dir/target_value"
+	echo "$now_psi_bp" > "$goal_dir/current_value"
+	echo commit_schemes_quota_goals > "$sysfs_dir/state"
 	sleep 1
 done
