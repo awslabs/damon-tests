1ced5fb899d0dc2f845ed508507c9a90183d161e
SeongJae Park <sj38.park@gmail.com>
2023-11-12 01:38:45 +0000

    perf/runners/back/0012_psi_tuning: Allow goal psi specified on var name
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- perf/runners/back/0012_psi_tuning.sh
+++ perf/runners/back/0012_psi_tuning.sh
@@ -8,9 +8,16 @@ bindir=$(dirname "$0")
 odir=$1
 var=$(basename $(dirname "$odir"))
 
-if ! echo "$var" | grep "_auto$" --quiet
+if ! echo "$var" | grep "_auto" --quiet
 then
 	exit 0
+fi
+
+# var names are XXX_auto_<goal_bp>
+psi_goal_bp=$(echo "$var" | awk -F_ '{print $3}')
+if [ "$psi_goal_bp" = "" ]
+then
+	psi_goal_bp=100
 fi
 
 sysfs_dir="/sys/kernel/mm/damon/admin/kdamonds/0/"
@@ -26,7 +33,7 @@ do
 
 	now_psi_bp=$(cat /proc/pressure/memory | grep some | awk '{print $2}' | \
 		awk -F= '{print $2 * 100}')
-	echo 100 > "$goal_dir/target_value"
+	echo psi_goal_bp > "$goal_dir/target_value"
 	echo "$now_psi_bp" > "$goal_dir/current_value"
 	echo commit_schemes_quota_goals > "$sysfs_dir/state"
 	sleep 1
