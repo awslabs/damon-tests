11bc75a6ba99a29da8cc2b6ee2654d48f00efa5d
SeongJae Park <sj38.park@gmail.com>
2023-11-18 17:21:08 +0000

    perf/runners/back/0012_psi_tuning: Make it works for only prcl_auto* vars
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- perf/runners/back/0012_psi_tuning.sh
+++ perf/runners/back/0012_psi_tuning.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Do feedback-based quota autotune for "*_auto*" variants.
+# Do feedback-based quota autotune for "prcl_auto*" variants.
 #
-# For prcl, var name is XXX_auto[_<goal_bp>].  The <goal_bp> means the target
-# value of last 10 secs memory some PSI, in bp (per 10K).  1% by default.
+# var name is XXX_auto[_<goal_bp>].  The <goal_bp> means the target value of
+# last 10 secs memory some PSI, in bp (per 10K).  1% by default.
 
 # $1: <...>/results/<exp>/<variance>/0(0-9)
 bindir=$(dirname "$0")
 odir=$1
 var=$(basename $(dirname "$odir"))
 
-if ! echo "$var" | grep "_auto" --quiet
+if ! echo "$var" | grep "prcl_auto" --quiet
 then
 	exit 0
 fi
