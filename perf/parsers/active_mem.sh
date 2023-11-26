121832805548657a942d677d7b8f7ab38d54bac0
sjpark <sj38.park@gmail.com>
2023-11-26 12:34:40 -0800

    perf/parsers: Intergrate inactive_mem in active_mem, and calculate active mem ratio
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- perf/parsers/active_mem.sh
+++ perf/parsers/active_mem.sh
@@ -3,5 +3,10 @@
 
 bindir=$(dirname "$0")
 
-average=$("$bindir/_average_nth_field.sh" 2 "$1/active_mem")
+active_average=$("$bindir/_average_nth_field.sh" 2 "$1/active_mem")
 echo "active_mem.avg: $average" > "$2/active_mem.avg"
+
+inactive_average=$("$bindir/_average_nth_field.sh" 2 "$1/inactive_mem")
+echo "inactive_mem.avg: $inactive_average" > "$2/inactive_mem.avg"
+
+echo "active_mem_rate_bp: $((active_average * 10000 / (active_average + inactive_average)))" > "$2/active_mem_rate_bp"
