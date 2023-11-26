5314e2652f4dbeed7c4aaa8e59c615d423225d51
sjpark <sj38.park@gmail.com>
2023-11-26 09:33:48 -0800

    perf/fetch_remote_results_mk_reports: Organize results with kernel version
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- perf/fetch_remote_results_mk_reports.sh
+++ perf/fetch_remote_results_mk_reports.sh
@@ -19,7 +19,9 @@ nr_hosts=$(($# - 4))
 hosts=(${@:4:$nr_hosts})
 dst_dir="${@: -1}"
 
-dst_dir="$dst_dir/$(date +%Y-%m-%d-%H-%M-%S)/"
+orig_dst_dir=$dst_dir
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+tmp_dst_dir="$orig_dst_dir/$timestamp/"
 
 if [ ! -f "$CFG" ]
 then
@@ -27,9 +29,9 @@ then
 	exit 1
 fi
 
-if [ -d "$dst_dir" ]
+if [ -d "$tmp_dst_dir" ]
 then
-	echo "destination dir $dst_dir already exist"
+	echo "destination dir $tmp_dst_dir already exist"
 	exit 1
 fi
 
@@ -52,30 +54,40 @@ do
 	echo "	$username@$host of port $port"
 done
 
-mkdir -p $dst_dir
+mkdir -p $tmp_dst_dir
 for host in "${hosts[@]}"
 do
 	rsync -arvz -e "ssh -p $port" --progress \
-		"$username@$host:$remote_results_path/" "$dst_dir/results.$host"
+		"$username@$host:$remote_results_path/" "$tmp_dst_dir/results.$host"
 done
 
-echo "aggregate the results at $dst_dir/results"
-"$aggregate_results" "$dst_dir"/* "$dst_dir/results"
+echo "aggregate the results at $tmp_dst_dir/results"
+"$aggregate_results" "$tmp_dst_dir"/* "$tmp_dst_dir/results"
 
 echo "remove raw results that not aggregated"
 for host in "${hosts[@]}"
 do
-	rm -fr "$dst_dir/results.$host"
+	rm -fr "$tmp_dst_dir/results.$host"
 done
 
-"$bindir/mk_visual_report.sh" "$dst_dir/results" "$dst_dir/visual_report"
+kernel_version=$(cat "$tmp_dst_dir"/results/*/*/*/*/kernel_version | head -n 1)
+final_dst_dir="$orig_dst_dir/$kernel_version/$timestamp"
+if [ -d "$final_dst_dir" ]
+then
+	echo "$final_dst_dir exist.  Temporal results is at $tmp_dst_dir"
+	exit 1
+fi
+mkdir -p "$final_dst_dir"
+mv "$tmp_dst_dir"/* "$final_dst_dir"/
+
+"$bindir/mk_visual_report.sh" "$final_dst_dir/results" "$final_dst_dir/visual_report"
 
 tmp_cfg="$CFG"_for_"$(date +%Y-%m-%d-%H-%M-%S)"
 cp "$CFG" "$tmp_cfg"
-echo "ODIR_ROOT=$dst_dir/results" >> "$tmp_cfg"
+echo "ODIR_ROOT=$final_dst_dir/results" >> "$tmp_cfg"
 export CFG="$tmp_cfg"
 "$bindir/post.sh"
 "$bindir/mk_reports.sh"
-mv report "$dst_dir/"
+mv report "$final_dst_dir/"
 
 rm "$tmp_cfg"
