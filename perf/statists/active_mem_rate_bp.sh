e36f4518c68184f66f48cf3f449a4a0e251965dc
sjpark <sj38.park@gmail.com>
2023-11-26 12:35:22 -0800

    perf/statists: Add a script for active memory ratio statistic
    Signed-off-by: sjpark <sj38.park@gmail.com>

---
+++ perf/statists/active_mem_rate_bp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+if [ $# -lt 2 ]
+then
+	echo "Usage: $0 <output directory> <src directory [src directory ...]>"
+	exit 1
+fi
+
+LBX="$(dirname "$0")/../../../lazybox"
+
+ODIR=$1
+SDIR=${@:2}
+
+for stat in avg min max stdev
+do
+	STATDIR=$ODIR/$stat
+	mkdir -p $STATDIR
+
+	f=active_mem_rate_bp
+	paths=""
+	for d in $SDIR
+	do
+		paths+=$d/$f" "
+	done
+
+	$LBX/scripts/report/statof.py $stat $paths > $STATDIR/$f
+done
