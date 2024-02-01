4241967fadbc68e0dfddc2da4f94dd70087b2e7b
SeongJae Park <sj38.park@gmail.com>
2024-02-01 09:47:05 -0800

    perf/setup_machine: Implement setup part except Parsec/Splash-2x install
    Reuse the local machine setup script for cont/ for easy basic setup.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- perf/setup_machine.sh
+++ perf/setup_machine.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Setup test machine
+
+set -e
+
+bindir=$(dirname "$0")
+repos_dir=$(realpath "$bindir/../../")
+
+# Setup test machine for corr test run
+cont_local_setup_sh=$(realpath "$bindir/../cont/_local_setup.sh")
+"$cont_local_setup_sh" "$repos_dir" upstream/next upstream/next gh.damon/next \
+	gh.damon https://github.com/damonitor/linux.git
 
 # Install PARSEC3/SPLASH-2X
-
-# Install lazybox and damo
-
-# Install kernel
