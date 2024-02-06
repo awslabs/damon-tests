76395bc0198e04d58f5f3c9ce694dcd85803f61f
SeongJae Park <sj38.park@gmail.com>
2024-02-05 17:14:56 -0800

    perf/setup_machine.sh: Receive kernel commit, remote name, and remote url
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- perf/setup_machine.sh
+++ perf/setup_machine.sh
@@ -2,18 +2,29 @@
 
 set -e
 
+if [ $# -ne 3 ]
+then
+	echo "Usage: $0 <kernel commit> <remote name> <remote url>"
+	echo
+	echo "Setup machine for running the performance test agains the"
+	echo "linux kernel of <kernel commit>, which can be fetched from"
+	echo "<remote name> remote repo of url <remote url>"
+	echo
+	echo "e.g., $0 985236144211 gh.damon https://github.com/damonitor/linux.git"
+	exit 1
+fi
+
+commit=$1
+remote=$2
+url=$3
+
 bindir=$(dirname "$0")
 repos_dir=$(realpath "$bindir/../../")
 
 # Setup test machine for corr test run
-linux_dir="$repos_dir/linux"
-if [ -d "$linux_dir" ]
-then
-	git -C "$linux_dir" fetch gh.damon
-fi
 cont_local_setup_sh=$(realpath "$bindir/../cont/_local_setup.sh")
-"$cont_local_setup_sh" "$repos_dir" upstream/next upstream/next gh.damon/next \
-	gh.damon https://github.com/damonitor/linux.git
+"$cont_local_setup_sh" "$repos_dir" upstream/next upstream/next \
+	"$commit" "$remote" "$url"
 
 # Install PARSEC3/SPLASH-2X
 cd "$repos_dir"
