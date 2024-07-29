5ce503554327e6e17b81bc08a195aa8c995cff32
SeongJae Park <sj@kernel.org>
2024-07-29 11:46:46 -0700

    corr/install_deps: Install libgmp and libmpc
    From Linux v6.11-rc1, the two librarires are required for arm64 build.
    Install those.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- corr/install_deps.sh
+++ corr/install_deps.sh
@@ -5,3 +5,6 @@ then
 	# python package is not in Ubuntu 22.04
 	sudo apt install -y xz-utils lftp python-is-python3
 fi
+
+# required from v6.11-rc1 for arm64 build
+sudo apt install -y libgmp-dev libmpc-dev
