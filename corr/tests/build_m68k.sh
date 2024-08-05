38d28c34b84148602482e6df8e71305b1b88ee25
Andrew Paniakin <apanyaki@amazon.com>
2024-08-05 01:13:23 +0000

    Fix cross-compile tests
    The old path to the make.cross file does not exist anymore,
    wget was creating empty file and it's execution returned 0 all the time.
    
    New make.cross tool uses scripts and configs in lkp-tests/kbuild folder, need
    to clone full repo.
    
    New make.cross tool defines a URL option - path to a compilers collection
    folder. Instead of getting lftp list for the full tree [1] it will only scan
    for the version that we need [2]. This reduces lftp scan time from 3 minutes to
    seconds.
    
    [1] https://cdn.kernel.org/pub/tools/crosstool/files/bin/x86_64
    [2] https://cdn.kernel.org/pub/tools/crosstool/files/bin/x86_64/9.3.0
    
    Signed-off-by: Andrew Paniakin <apanyaki@amazon.com>

--- corr/tests/build_m68k.sh
+++ corr/tests/build_m68k.sh
@@ -12,10 +12,12 @@ ODIR=$TESTDIR/`basename $0`.out
 mkdir -p bin
 PATH=$TESTDIR/bin/:$PATH
 
-if [ ! -x ./bin/make.cross ]
+PATH=$TESTDIR/bin/lkp-tests/kbuild/:$PATH
+
+if [ ! -x ./bin/lkp-tests/kbuild/make.cross ]
 then
-	wget https://raw.githubusercontent.com/intel/lkp-tests/master/sbin/make.cross -O ./bin/make.cross
-	chmod +x ./bin/make.cross
+	git clone https://github.com/intel/lkp-tests ./bin/lkp-tests
+	chmod +x ./bin/lkp-tests/kbuild/make.cross
 fi
 
 mkdir -p $ODIR
@@ -26,7 +28,8 @@ echo 'CONFIG_MODULES=y' >> $ODIR/.config
 cat "$TESTDIR/damon_config" >> "$ODIR/.config"
 
 export COMPILER_INSTALL_PATH=$HOME/0day
-export GCC_VERSION=7.5.0
+export COMPILER=gcc-7.5.0
+export URL=https://cdn.kernel.org/pub/tools/crosstool/files/bin/x86_64/7.5.0
 
 make.cross O=$ODIR ARCH=m68k olddefconfig
 make.cross O=$ODIR ARCH=m68k -j`grep -e '^processor' /proc/cpuinfo | wc -l`
